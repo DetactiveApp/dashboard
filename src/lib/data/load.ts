8f439d1479997ca7f285963ab005f2eb1d3d235f
Max-Beier <max@maxbeier.dev>
2023-11-18 20:51:34 +0100

    remove logs

--- src/lib/data/load.ts
+++ src/lib/data/load.ts
@@ -69,8 +69,6 @@ const load = async (storyUuid: string) => {
                 connections.push([decision.stepInputUuid!, decision.stepOutputUuid!, decision.uuid!]);
             }
         }
-
-        console.log(connections)
     }
 
     // Update data & get anchors
